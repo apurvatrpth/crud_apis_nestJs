b6f64fa4777b4024e98f7a02092da10c174800c9
Apurva Tripathi <at131296@gmail.com>
2020-06-19 02:38:06 +0530

    Update Keys.ts to remove password
    Replace <enterPasswordHere> in keys.ts with actual cluster password.

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,3 +1,3 @@
 export default {
-    mongoUri: 'mongodb+srv://apurva:9919293789@cluster0-epo2e.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    mongoUri: 'mongodb+srv://apurva:<enterPasswordHere>@cluster0-epo2e.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority',
 };
